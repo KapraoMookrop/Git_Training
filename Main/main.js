19bcaec9c9047962c54c2679d94672dac033ffcf
Chatchawan Taja <chatchawant03@gmail.com>
2026-08-09 17:32:20 +0700

    main script

---
+++ Main/main.js
@@ -0,0 +1,11 @@
+function calculateVat(price) {
+    let vat = price * 0.07;
+    let total = price + vat;
+    return total;
+}
+
+let productPrice = 500;
+let netTotal = calculateVat(productPrice);
+
+console.log("Price: " + productPrice + " บาท");
+console.log("Total with VAT 7%: " + netTotal + " บาท");
