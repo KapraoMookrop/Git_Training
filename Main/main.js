318727b62ce7ac7883392f38eb4398f5b0a01a54
Chatchawan Taja <chatchawant03@gmail.com>
2026-08-09 17:34:18 +0700

    commit main

--- Main/main.js
+++ Main/main.js
@@ -4,6 +4,13 @@ function calculateVat(price) {
     return total;
 }
 
+function calculateVat_10Per(price) {
+    let vat = price * 0.1;
+    let total = price + vat;
+    return total;
+}
+
+
 let productPrice = 500;
 let netTotal = calculateVat(productPrice);
 
